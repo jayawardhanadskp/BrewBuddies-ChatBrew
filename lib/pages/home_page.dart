97bae95f5c6a4f30c7a6bc3b422d13b6d9827b27
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-20 15:54:13 +0530

    home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:brewbuddies_chat/utils/app_colors.dart';
-import 'package:brewbuddies_chat/utils/app_fonts.dart';
 import 'package:brewbuddies_chat/widgets/coffee_small_widget.dart';
+import 'package:brewbuddies_chat/widgets/home_widgets.dart';
 import 'package:brewbuddies_chat/widgets/tab_content.dart';
 import 'package:flutter/material.dart';
 
@@ -35,57 +35,7 @@ class _HomePageState extends State<HomePage>
         children: [
           Column(
             children: [
-              const SizedBox(height: 70),
-              Image.asset('assets/HeadNav.png'),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Find the best\nCoffee to your taste',
-                        style: AppFonts.large.copyWith(color: AppColors.darkGreay),
-                      ),
-                    ),
-                    const SizedBox(height: 30),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Container(
-                            height: 55,
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: AppColors.lightGreay, width: 1),
-                                borderRadius: BorderRadius.circular(15),
-                                color: Colors.white),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 20.0, vertical: 15),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Image.asset('assets/Search.png', height: 20),
-                                  const SizedBox(width: 10),
-                                  Text(
-                                    'Find your coffee...',
-                                    style: AppFonts.small
-                                        .copyWith(color: AppColors.lightGreay),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Image.asset('assets/sort.png', height: 55)
-                      ],
-                    ),
-                    const SizedBox(height: 30),
-                  ],
-                ),
-              ),
+              const HomeWidgets(),
               TabBar(
                 controller: _tabController,
                 isScrollable: true,
@@ -109,72 +59,31 @@ class _HomePageState extends State<HomePage>
               Expanded(
                 child: TabBarView(
                   controller: _tabController,
-                  children: [
-                    TabContent(title:'Espresso'),
-                    TabContent(title:'Latte'),
-                    TabContent(title:'Cappuccino'),
-                    TabContent(title:'Cafetière'),
+                  children: const [
+                    TabContent(title: 'Espresso'),
+                    TabContent(title: 'Latte'),
+                    TabContent(title: 'Cappuccino'),
+                    TabContent(title: 'Cafetière'),
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Special for you',
-                        style: AppFonts.large.copyWith(color: AppColors.darkGreay),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Container(
-                      height: 150,
-                      width: double.infinity,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: AppColors.normalBrown,
-                        borderRadius: BorderRadius.circular(25),
-                      ),
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: 180,
-                            height: double.infinity,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(25),
-                              child: Image.asset(
-                                'assets/Creamy.png',
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 15),
-                          Expanded(
-                            child: Text(
-                              'Specially mixed and brewed whivh you must try!',
-                              style: AppFonts.small.copyWith(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              )
+              const HomeWidgetsDown(),
             ],
+          ),
+          Positioned(
+            bottom: 100,
+            right: 10,
+            child: Container(
+              height: 80,
+              width: 80,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: AppColors.lightBrown,
+              ),
+            ),
           ),
         ],
       ),
     );
   }
-
-  
 }
