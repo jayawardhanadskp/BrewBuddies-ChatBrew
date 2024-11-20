2c5916569cf90ea2e8d09527b7bcc3fba88f7aa9
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-20 10:43:08 +0530

    app fonts

---
+++ lib/utils/app_fonts.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppFonts {
+  static TextStyle large = GoogleFonts.roboto(fontSize: 24, fontWeight: FontWeight.w600);
+  static TextStyle extraSmall = GoogleFonts.roboto(fontSize: 14, );
+  
+  
+  static TextStyle small = GoogleFonts.roboto(fontSize: 16, );
+}
