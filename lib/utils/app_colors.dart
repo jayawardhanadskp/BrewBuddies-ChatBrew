7a991611ff579c9b13f3cc90b7bad8b7a1c6db0f
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-20 14:08:45 +0530

    assets

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const Color background = Color(0xFFEFEFEF);
   static const lightBrown = Color(0xFFECE0D1);
   static const normalBrown = Color(0xFF967259);
   static const darkBrown = Color(0xFF38220F);
   static const lightGreay = Color(0xFFCCCCCC);
+  static const darkGreay = Color(0xFF444444);
 
 }
