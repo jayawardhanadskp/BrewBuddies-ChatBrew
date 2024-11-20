98452bd5def77d8dbeedfd7aa53fdaf5d6f7faa6
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-20 10:43:16 +0530

    app colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const lightBrown = Color(0xFFECE0D1);
+  static const normalBrown = Color(0xFF967259);
+  static const darkBrown = Color(0xFF38220F);
+
+}
