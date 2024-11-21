aef7de1c59ea110a614956a38d0a7828c42bdd18
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-21 11:52:01 +0530

    chat provider

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -1,23 +1,27 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
-class ChatProvider extends ChangeNotifier {
+class ChatState with ChangeNotifier {
   final List<Map<String, String>> _chatHistory = [];
   bool _isChatVisible = false;
 
   List<Map<String, String>> get chatHistory => _chatHistory;
   bool get isChatVisible => _isChatVisible;
 
-  // add messages to the chat history
+  void toggleChatVisibility() {
+    _isChatVisible = !_isChatVisible;
+    if (!_isChatVisible) {
+      clearChat();
+    }
+    notifyListeners();
+  }
+
   void addMessage(String sender, String message) {
     _chatHistory.add({'sender': sender, 'message': message});
     notifyListeners();
   }
 
-  void toggleChatVisibility() {
-    _isChatVisible = !_isChatVisible;
-    if (!_isChatVisible) {
-      _chatHistory.clear();  // Clear chat history when closed
-    }
+  void clearChat() {
+    _chatHistory.clear();
     notifyListeners();
   }
 }
